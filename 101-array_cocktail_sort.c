d5d3031e1f39bdadbf79a62705aa702951bf7493
Ambesawi <ambesawi@gmail.com>
2023-04-25 16:23:48 -0700

    101-cocktail_sort_list.c

---
+++ 101-array_cocktail_sort.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_array - print the array to perform print_array.
+ * @array: array to beprinted.
+ * @size: size of the array.
+ * Return: nothing
+ */
+
+void print_array(const int *array, size_t size)
+{
+	size_t j;
+	
+	j = 0;
+	while (array && j < size)
+	{
+		if (j > 0)
+			printf(", ");
+		printf("%d", array[j]);
+		++j;
+	}
+	printf("\n");
+}
+
+/**
+ * cocktail_sort - sort the array to perform cocktail_sort.
+ * @array: array to be sorted in cocktail_sort.
+ * @size: size of the array.
+ * Return: nothing
+ */
+void cocktail_sort(int *array, size_t size)
+{
+	int swaped = 1;
+	size_t i = 0, j = size - 1, k;
+	int swap_p;
+
+	while (i < j && swaped)
+	{
+		swaped = 0;
+		for (k = i; k < j; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				swap_p = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = swap_p;
+				print_array(array, size);
+				swaped = 1;
+			}
+		}
+		j--;
+		if (swaped)
+		{
+			swaped = 0;
+			for (k = j; k > i; k--)
+			{
+				if (array[k] < array[k - 1])
+				{
+					swap_p = array[k];
+					array[k] = array[k - 1];
+					array[k - 1] = swap_p;
+					print_array(array, size);
+					swaped = 1;
+				}
+			}
+		}
+		i++;
+	}
+}
+
+int main(void)
+{
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	print_array(array, n);
+	printf("\n");
+	cocktail_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
+}
--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,83 +1,67 @@
-#include <stdio.h>
-#include <stddef.h>
+#include "sort.h"
 
 /**
- * print_array - print the array to perform print_array.
- * @array: array to beprinted.
- * @size: size of the array.
+ * swap - swap nodes for exchange
+ * @a: first node that is to be swaped.
+ * @b: second node that is to be swapped.
  * Return: nothing
  */
-
-void print_array(const int *array, size_t size)
+void swap_l(listint_t *a, listint_t *b)
 {
-	size_t j;
-	
-	j = 0;
-	while (array && j < size)
-	{
-		if (j > 0)
-			printf(", ");
-		printf("%d", array[j]);
-		++j;
-	}
-	printf("\n");
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 }
-
 /**
- * cocktail_sort - sort the array to perform cocktail_sort.
- * @array: array to be sorted in cocktail_sort.
- * @size: size of the array.
- * Return: nothing
+ * cocktail_sort_list - perform sorting using cocktail_sort_list.
+ * @list: list to be sorted in cocktail_sort_list.
+ * Return: nothing.
  */
-void cocktail_sort(int *array, size_t size)
+void cocktail_sort_list(listint_t **list)
 {
-	int swaped = 1;
-	size_t i = 0, j = size - 1, k;
-	int swap_p;
+	listint_t *current_t;
+	int swapped = 1;
 
-	while (i < j && swaped)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current_t = *list;
+	while (swapped)
 	{
-		swaped = 0;
-		for (k = i; k < j; k++)
+		swapped = 0;
+		while (current_t && current_t->next)
 		{
-			if (array[k] > array[k + 1])
+			if (current_t->n > current_t->next->n)
 			{
-				swap_p = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = swap_p;
-				print_array(array, size);
-				swaped = 1;
+				swap_l(current_t, current_t->next);
+				if (current_t->prev->prev == NULL)
+					*list = current_t->prev;
+				print_list(*list);
+				swapped = 1;
 			}
+			else
+				current_t = current_t->next;
 		}
-		j--;
-		if (swaped)
+		if (!swapped)
+			break;
+		swapped = 0;
+		while (current_t && current_t->prev)
 		{
-			swaped = 0;
-			for (k = j; k > i; k--)
+			if (current_t->n < current_t->prev->n)
 			{
-				if (array[k] < array[k - 1])
-				{
-					swap_p = array[k];
-					array[k] = array[k - 1];
-					array[k - 1] = swap_p;
-					print_array(array, size);
-					swaped = 1;
-				}
+				swap_l(current_t->prev, current_t);
+				if (current_t->prev == NULL)
+					*list = current_t;
+				print_list(*list);
+				swapped = 1;
 			}
+			else
+				current_t = current_t->prev;
 		}
-		i++;
 	}
-}
-
-int main(void)
-{
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
-
-	print_array(array, n);
-	printf("\n");
-	cocktail_sort(array, n);
-	printf("\n");
-	print_array(array, n);
-	return (0);
 }
