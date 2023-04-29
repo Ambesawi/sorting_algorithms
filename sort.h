d7ffdc481aae8aceed89d34baefb37eb2974223e
Ambesawi <ambesawi@gmail.com>
2023-04-29 15:23:01 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -31,5 +31,9 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void bitcompare(int *array, int i, int j, int dir);
+void bitmerge(int *array, int low, int cnt, int dir);
+void bitsort(int *array, int low, int cnt, int dir, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif
